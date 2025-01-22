4a617610705f735ab10bb8eda3772c39b5a1a76b
mbakhouc <bakhouch2019@gmail.com>
2025-01-22 13:52:40 +0100

    new notif ring

--- backend/internal/route/route.go
+++ backend/internal/route/route.go
@@ -58,7 +58,7 @@ func SetupPageRoutes(mux *http.ServeMux) {
 			"imgs/s.png":             true,
 			"imgs/avatar.png":        true,
 			"imgs/backgtoundWeb.png": true,
-			"imgs/notif.png": true,
+			"imgs/message.gif": true,
 		}
 
 		if !allowedFiles[suffix] {
